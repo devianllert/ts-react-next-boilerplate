aebdc85f9d8ae3cd8b83190ab33e9649af25afcd
devianllert <devianllert@gmail.com>
2019-12-08 21:07:16 +0300

    refactor: rewrite pages to TS

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -3,15 +3,16 @@ import React from 'react';
 import { Provider } from 'react-redux';
 
 import Head from 'next/head';
-import App from 'next/app';
+import App, { AppProps } from 'next/app';
+import { Store } from 'redux';
 
-import withReduxStore from 'utils/with-redux-store';
-import { appWithTranslation } from 'utils/with-i18next';
+import withReduxStore from '../utils/with-redux-store';
+import { appWithTranslation } from '../utils/with-i18next';
 
 import 'typeface-metropolis';
 import '@typefaces-pack/typeface-inter';
 
-class Srr extends App {
+class Srr extends App<AppProps & { reduxStore: Store }> {
   render() {
     const { Component, pageProps, reduxStore } = this.props;
     return (
--- app/pages/_document.tsx
+++ app/pages/_document.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Global, css } from '@emotion/core';
 
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
+  static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
+
     return { ...initialProps };
   }
 
